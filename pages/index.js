c885985208a180a05e32a25e63924ef1b999e877
Drew Tada <drew@builders-lt10.myfiosgateway.com>
2021-02-03 17:44:28 -0500

    initialized next app with ts

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <main>
+        test
+      </main>
+
+      <footer>
+
+      </footer>
+    </>
+  )
+}
